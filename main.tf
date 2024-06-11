f3f1bfe43210e3331590dcb555b8c20593b7482f
AthiraJoy1 <joyathira2611@gmail.com>
2024-06-11 15:40:12 +0530

    Add files via upload
    Changes

---
+++ main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "aks-rg1" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-rg1.name
+  dns_prefix          = var.cluster_name
+
+  default_node_pool {
+    name                = "system"
+    node_count          = var.system_node_count
+    vm_size             = "standard_ds2_v2"
+    type                = "VirtualMachineScaleSets"
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet" 
+  }
+}
